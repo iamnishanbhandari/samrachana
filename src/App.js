98c4ac87a2b016b8c021cd3cc80f0567f29d22ef
Nishan Bhandari <bnishan18@gmail.com>
2023-10-05 22:36:47 +0545

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/navbar/NavBar";
+import { Box } from "@mui/material";
+import Footer from "./components/footer/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box sx={{ backgroundColor: "#0F1038", height: "100vh" }}>
+        <NavBar />
+        {/* <Footer /> */}
+      </Box>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <Box sx={{ marginTop: "100px" }}>hello its me footer</Box>
+    </>
+  );
+};
+
+export default Footer;
